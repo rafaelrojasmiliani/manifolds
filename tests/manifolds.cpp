73901ecbb85d48e00c83f093e5ebe55728c48e26
Rojas Rafael <rafael.rojas@unibz.it>
2022-08-02 16:31:56 +0200

    (#1) SO3 test parametrizations

--- tests/manifolds.cpp
+++ tests/manifolds.cpp
@@ -45,9 +45,9 @@ Eigen::Matrix<double, 3, 1> euler_zyz_chart(const SO3 &_obj) {
   const Eigen::Isometry3d &_mat = _obj.get_representation();
 
   Eigen::Matrix<double, 3, 1> result;
-  result(0) = std::atan2(_mat(1, 0), _mat(0, 0));
-  result(1) = std::atan2(-_mat(2, 0), std::hypot(_mat(2, 1), _mat(2, 2)));
-  result(2) = std::atan2(_mat(2, 1), _mat(2, 2));
+  result(0) = std::atan2(_mat(1, 2), _mat(0, 2));
+  result(1) = std::atan2(std::hypot(_mat(0, 2), _mat(1, 2)), _mat(2, 2));
+  result(2) = std::atan2(_mat(2, 1), -_mat(2, 0));
   return result;
 }
 
@@ -56,20 +56,23 @@ SO3 euler_zyz_parametrization(const Eigen::Matrix<double, 3, 1> &_in) {
       Eigen::Translation3d(0, 0, 0) *
       Eigen::Quaterniond(Eigen::AngleAxisd(_in(0), Eigen::Vector3d::UnitZ()) *
                          Eigen::AngleAxisd(_in(1), Eigen::Vector3d::UnitY()) *
-                         Eigen::AngleAxisd(_in(2), Eigen::Vector3d::UnitX()));
+                         Eigen::AngleAxisd(_in(2), Eigen::Vector3d::UnitZ()));
 
   return result;
 }
 
 TEST(Basis, Get_Basis) {
 
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < 100; i++) {
 
     Eigen::Vector3d v = Eigen::Vector3d::Random();
 
     Eigen::Vector3d v2 = euler_rpy_chart(euler_rpy_parametrization(v));
-
     ASSERT_LT((v - v2).norm(), 1.0e-10);
+
+    v(1) += 1.0;
+    v2 = euler_zyz_chart(euler_zyz_parametrization(v));
+    ASSERT_LT((v - v2).norm(), 1.0e-10) << "ZYZ Euler";
   }
 }
 
